e596fdc1ae554fcb3e7474f34c5d3740122f3f04
sofiaM <smejia03@gmail.com>
2020-06-20 22:21:07 -0500

    menu bar & API added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 // import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { Container } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import Nav from "./components/Nav";
+
+const headerStyle = {
+  textAlign: "center",
+};
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <div className="App">
-        {/* <header className="App-header">
-         <h1 className="center-align">Google Books Search</h1>
-      </header> */}
-        <div>
-          <h2>hola mundo!</h2>
-          <Button variant="contained" color="secondary">
-            Hello World
-          </Button>
-        </div>
-      </div>
-    </Container>
+    <Router>
+      <Container>
+        <Grid container xs={12}>
+          <Nav />
+          <Grid item xs={12} style={headerStyle}>
+            <h1>Search & Save Books of Interest</h1>
+          </Grid>
+        </Grid>
+      </Container>
+    </Router>
   );
 }
 
