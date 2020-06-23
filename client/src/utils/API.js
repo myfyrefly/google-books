b69223d0523f03907ece6102a6139d1bee9b7a90
sofiaM <smejia03@gmail.com>
2020-06-22 20:47:14 -0500

    changed to bootstrap instead of material ui

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,22 +1,32 @@
 import axios from "axios";
+require("dotenv").config();
+const apiKey = process.env.REACT_APP_API_KEY;
 
-const API = {
-  // Gets books from the Google API
-  getBooks: function (q) {
-    return axios.get("/api/google", { params: { q: "title:" + q } });
-  },
-  // Gets all saved books
-  getSavedBooks: function () {
+// const myApiKey = process.env.API_KEY;
+
+export default {
+  // Gets all books
+  getBooks: function () {
     return axios.get("/api/books");
   },
-  // Deletes the saved book with the given id
+  // Gets the book with the given id
+  getBook: function (id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
   deleteBook: function (id) {
     return axios.delete("/api/books/" + id);
   },
-  // Saves an book to the database
+  // Saves a book to the database
   saveBook: function (bookData) {
+    console.log("bookData: ");
+    console.log(bookData);
     return axios.post("/api/books", bookData);
   },
+  searchBook: function (bookData) {
+    console.log(bookData);
+    return axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${bookData}&key=AIzaSyC9Tw6Y-gcvz_J5jQHOSe5gcd31M_QWC-s`
+    );
+  },
 };
-
-export default API;
